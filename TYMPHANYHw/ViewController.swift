82c236d42edefc060cdbe3bd16bf827ba0c8d4ad
ming.gu <ming.gu@gearradio.net>
2022-02-12 09:28:57 +0800

    Initial Commit

---
+++ TYMPHANYHw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TYMPHANYHw
+//
+//  Created by  明智 on 2022/2/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
